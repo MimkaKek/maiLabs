8b19f93bf958d89c7565507ecd870b1cbcc0a9f5
MimkaKek <grislava@gmail.com>
2019-07-29 16:01:38 +0300

    Lab02 final state

--- mai_oop/Lab02/TArray.cpp
+++ mai_oop/Lab02/TArray.cpp
@@ -4,29 +4,72 @@
 
 TArray::TArray() {
 	amount = 0;
-	array = NULL;
+	array = nullptr;
 }
 
 void TArray::Add(std::shared_ptr<Figure> figure) {
-	if(amount == 0) {
-		array = (std::shared_ptr<Figure>*) malloc((++amount) * sizeof(std::shared_ptr<Figure>));
-		array[0] = figure;
-		return;
+	
+    if(amount == 0) {
+		array  = new std::shared_ptr<Figure>;
+		*array = figure;
 	}
-	else {
-		array = (std::shared_ptr<Figure>*) realloc(array, (++amount) * sizeof(std::shared_ptr<Figure>));
-		array[amount - 1] = figure;
-		return;
-	}
+    else {
+        std::shared_ptr<Figure>* tmp = new std::shared_ptr<Figure>[amount + 1];
+        for(int step = 0; step < amount; step++) {
+            tmp[step]   = array[step];
+            array[step] = nullptr;
+        }
+        tmp[amount] = figure;
+        if(amount == 1) {
+            delete array;    
+        }
+        else {
+            delete[] array;
+        }
+        array = tmp;
+    }
+    
+    ++amount;
+    return;
 }
 
 std::shared_ptr<Figure> TArray::Remove(int i) {
-	std::shared_ptr<Figure> tmp;
-	tmp = array[i - 1];
-	array[i - 1] = array[amount - 1];
-  array[amount - 1] = nullptr;
-	array = (std::shared_ptr<Figure>*) realloc(array, (--amount) * sizeof(std::shared_ptr<Figure>));
-	return tmp;
+	
+    
+    std::shared_ptr<Figure>* tmp    = nullptr;
+    std::shared_ptr<Figure>  result = nullptr;
+    
+	if((i < 1 || i > amount) || !amount) {
+        return result;
+    }
+    
+    result            = array[i - 1];
+	array[i - 1]      = array[amount - 1];
+    array[amount - 1] = nullptr;
+	
+    if(amount > 1) {
+        
+        if(amount == 2) {
+            tmp = new std::shared_ptr<Figure>;
+        }
+        else {
+            tmp = new std::shared_ptr<Figure>[amount - 1];
+        }
+        
+        for(int step = 0; step < amount - 1; step++) {
+            tmp[step]   = array[step];
+            array[step] = nullptr;
+        }
+        delete[] array;
+        array = tmp;
+    }
+    else {
+        delete array;
+        array = nullptr;
+    }
+    
+    --amount;
+    return result;
 }
 
 unsigned int TArray::GetAmount() const {
@@ -46,8 +89,20 @@ std::ostream& operator<<(std::ostream& os, TArray& array) {
 	return os;
 }
 TArray::~TArray() {
-  for(int i = 0; i < amount; i++)
-    array[i] = nullptr;
-	delete array;
+    
+    if(amount) {
+        for(int i = 0; i < amount; i++) {
+            array[i] = nullptr;
+        }
+        
+        if(amount == 1) {
+            delete array;
+        }
+        else {
+            delete[] array;
+        }
+    }
+	
 	std::cout << "Array destroyed!" << std::endl;
+
 }
--- mai_oop/Lab02/TArray.h
+++ mai_oop/Lab02/TArray.h
@@ -7,15 +7,21 @@
 class TArray {
 public:
 	TArray();
-	void Add(std::shared_ptr<Figure> figure);
-	std::shared_ptr<Figure> Remove(int i);
-  void Print(int step);
-	unsigned int GetAmount() const;
-	friend std::ostream& operator<<(std::ostream& os, TArray& array);
-	virtual ~TArray();
+	
+    void                        Add(std::shared_ptr<Figure> figure);
+    
+	std::shared_ptr<Figure>     Remove(int i);
+    
+    void                        Print(int step);
+	
+    unsigned int                GetAmount() const;
+	
+    friend std::ostream& operator << (std::ostream& os, TArray& array);
+	
+    virtual ~TArray();
 private:
-	unsigned int amount;
-  std::shared_ptr<Figure>* array;
+	unsigned int                amount;
+    std::shared_ptr<Figure>*    array;
 };
 
 #endif	/* TARRAY_H */
